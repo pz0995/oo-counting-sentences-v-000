20f4ef0618e1f033c2abb1340038d7ed3594e724
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-26 04:40:20 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-      self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
   end
 end
